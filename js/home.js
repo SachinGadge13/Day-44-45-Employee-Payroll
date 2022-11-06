a75b9d9cba4cb6e18daf8a908ab3a96f2856f1c3
SachinGadge13 <gadges14@gmail.com>
2022-11-06 17:08:36 +0530

    Added:Employee Payroll details in Tabular Format

---
+++ js/home.js
@@ -0,0 +1,69 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    createInnerHtml();
+});
+
+//Template literal ES6 feature
+const createInnerHtml = () => {
+    const headerHtml = "<tr><th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th>" +
+        "<th>Start Date</th><th>Actions</th></tr>"
+   
+    
+    let innerHtml = `${headerHtml}`;
+    let empPayrollList = createEmployeePayrollJSON();
+    for(const empPayrollData of empPayrollList){
+        innerHtml=`${innerHtml}
+            <tr>
+                <td><img class="profile" src="${empPayrollData._profilePic}" alt=""></td>
+                <td>${empPayrollData._name}</td>
+                <td>${empPayrollData._gender}</td>
+                <td>${getDeptHtml(empPayrollData._department)}</td>
+                <td>${empPayrollData._salary}</td>
+                <td>${empPayrollData._startDate}</td>
+                <td>
+                    <img name="${empPayrollData._id}" onclick="remove(this)" src="/assets/icons/icons8-delete.svg" alt="delete">
+                    <img name="${empPayrollData._id}" onclick="update(this)" src="/assets/icons/edit.svg" alt="edit">
+                </td>
+            </tr>
+    `;
+    }
+    
+    document.querySelector('#table-display').innerHTML = innerHtml;
+}
+const createEmployeePayrollJSON = () => {
+    let empPayrollListLocal = [{
+            _name: 'Narayan Mahadevan',
+            _gender: 'male',
+            _department: [
+                'Engineering',
+                'Finance'
+            ],
+            _salary: '500000',
+            _startDate: '29 Oct 2019',
+            _note: '',
+            _id: new Date().getTime(),
+            _profilePic: "assets\images\Eclipse-2.PNG"
+        },
+        {
+            _name: 'Aparna Shashanka Keerthi Kumar',
+            _gender: 'female',
+            _department: [
+
+                'Sales'
+            ],
+            _salary: '400000',
+            _startDate: '29 Oct 2019',
+            _note: '',
+            _id: new Date().getTime() + 1,
+            _profilePic: "assets\images\Ellipse -1.png"
+        }
+    ];
+    return empPayrollListLocal;
+}
+
+const getDeptHtml = (deptList) =>{
+    let deptHtml = '';
+    for(const dept of deptList){
+        deptHtml=`${deptHtml}<div class='dept-label'>${dept}</div>`
+    }
+    return deptHtml;
+}
